e075e7e4baab84859c00d854789546a069b83023
Cameron Anderson and Katie Fujihara <cameron@>
2018-03-22 14:55:39 -0700

    add algorithm and promises

--- src/gitSome.js
+++ src/gitSome.js
@@ -1,36 +1,45 @@
 import $ from 'jquery';
 
-function gitSome(apiUrl, callback) {
-  $.ajax({
-    url: apiUrl,
-    data: {
-      format: 'json'
-    },
-    success: callback,
-    error: function() {
-      $('#errors').text("There was an error processing your request. Please try again.")
+function gitPromise(url) {
+  return new Promise(function(resolve, reject) {
+    let request = new XMLHttpRequest();
+    request.onload = function() {
+      if (this.status === 200) {
+        console.log(url);
+        let response = JSON.parse(request.response);
+        resolve(response);
+      } else {
+        reject(Error(request.statusText));
+      }
     }
+    request.open("GET", url, true);
+    request.send();
   });
 }
 
-function search(query, callback) {
-   gitSome(`https://api.github.com/search/users?q=${query}&per_page=150`, callback);
+function search(query) {
+   return gitPromise(`https://api.github.com/search/users?q=${query}&per_page=150`);
 }
 
-function getUser(username, callback) {
-  gitSome(`https://api.github.com/users/${username}`, callback);
+function getUser(username) {
+  return gitPromise(`https://api.github.com/users/${username}`);
 }
 
-function getMatches(location, followers, callback){
+function getMatches(location, followers, language){
   if (location == undefined){
-    gitSome(`https://api.github.com/search/users?q=followers:${followers-3}..${followers+3}&per_page=150`, callback);
+    return gitPromise(`https://api.github.com/search/users?q=Epicodus+followers:${followers-1}..${followers+1}+language:"${language}"&per_page=150`);
   }
-  else { gitSome(`https://api.github.com/search/users?q=followers:${followers-3}..${followers+3}+location:"${location}"&per_page=150`, callback);
+  else {
+     return gitPromise(`https://api.github.com/search/users?q=epicodus+followers:${followers-5}..${followers+5}+language:"${language}"&per_page=150`);
   }
+}
+
+function getRepo(username){
+  return gitPromise(`https://api.github.com/users/${username}/repos?sort=date&per_page=1`);
 }
 
 // function sameLang(username, language, callback){
-//   gitSome(`https://api.github.com/users/${username}
+//   gitpromise(`https://api.github.com/users/${username}`)
 // }
 
-export { gitSome, search, getUser, getMatches }
+export { search, getUser, getMatches, getRepo }
--- src/main.js
+++ src/main.js
@@ -1,41 +1,77 @@
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { gitSome, search, getUser, getMatches } from './gitSome.js';
+import { search, getUser, getMatches, getRepo } from './gitSome.js';
 
-function getInfo(user) {
-  getUser(user.login, function(response) {
-    getMatches(response.location, response.followers, function(matches){
-      $('#showUsers').text('');
-      $("#resultsFound").text(matches.total_count + " Results Found");
-      if (response.location == undefined){
-        $("#markLocation").append("<h3><strong>We recommend you put in a location to get a more accurate programmer suggestion!</strong></h3>")
-      }
-      matches.items.forEach(function(match) {
-        $('#showUsers').append(`<div class="col-md-4"><a href="${match.html_url}"><h3 class=loginName>${match.login}</h3></a><img src="${match.avatar_url}" id="profilepic">`);
+function searchUser(searchString) {
+  let promise = search(searchString);
+  promise.then(function(response) {
+    $('#showUsers').text('');
+    $("#resultsFound").text(response.total_count + " Result(s) Found");
+    response.items.forEach(function(user) {
+      $('#showUsers').append(`<div class="col-md-4" id="me"><a target="_blank" href="${user.html_url}"><h3 class=loginName>${user.login}</h3></a><img src="${user.avatar_url}" id="profilepic"><button type="button" class="btn btn-info matchMe">Match Me!</button><hr></div>`);
+      $('.matchMe').last().click(function() {
+        getUserInfo(user);
       });
     });
-  });
+  }, handleErrors);
+}
+
+function getUserInfo(user) {
+  let promiseInfo = getUser(user.login);
+  let promiseLang = getRepo(user.login);
+  Promise.all([promiseInfo, promiseLang]).then(function(values){
+    console.log(values);
+    console.log(values[0].login);
+    console.log(values[0].followers);
+    console.log(values[1][0].language);
+    getUserMatches(values[0].location, values[0].followers, values[1][0].language);
+  }, handleErrors);
+}
+
+function getUserMatches(location, followers, language) {
+  let promise = getMatches(location, followers, language);
+  promise.then(function(matches){
+    $('#showUsers').text('');
+    $("#resultsFound").text(matches.total_count + " Results Found");
+    if (location == undefined){
+      $("#markLocation").text("We recommend you put in a location in your Github profile to get a more accurate programmer suggestion!")
+    }
+    $('#matchrequest').click(function(){
+        $("#markLocation").text(" ")
+    });
+    matches.items.forEach(function(match) {
+      $('#showUsers').append(`<div class="col-md-4"><a target="_blank" href="${match.html_url}"><h3 class=loginName>${match.login}</h3></a><img src="${match.avatar_url}" id="profilepic">`);
+    });
+  }, handleErrors);
+}
+
+function handleErrors(error) {
+  console.log('something went horribly wrong: ' + error);
 }
 
 $(document).ready(function() {
-  $('#nameForm').submit(function(event) {
+  $('.nameForm').submit(function(event) {
+    event.preventDefault();
     $(".hidden").show();
     $("#codelines").hide();
     let searchString = $('#username').val();
     $('#username').val("");
-    search(searchString, function(response) {
-      $('#showUsers').text('');
-      $("#resultsFound").text(response.total_count + " Result(s) Found");
-      response.items.forEach(function(user) {
-        $('#showUsers').append(`<div class="col-md-4" id="me"><a href="${user.html_url}"><h3 class=loginName>${user.login}</h3></a><img src="${user.avatar_url}" id="profilepic"><button type="button" class="btn btn-info matchMe">Match Me!</button><hr></div>`);
-        $('.matchMe').last().click(function() {getInfo(user)});
-      });
-    });
-    event.preventDefault();
+    searchUser(searchString);
+
   });
   $("#matchrequest").click(function(event){
     $(".hidden").hide();
     $("#codelines").show();
+  });
+
+  $('#search').submit(function(event) {
+    event.preventDefault();
+    $(".hidden").show();
+    $("#codelines").hide();
+    let searchString = $('#searchbar').val();
+    $('#username').val("");
+    searchUser(searchString);
+
   });
 });
