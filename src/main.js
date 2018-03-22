671a4cf0558fc23ddd8ea684e8888ec206c09cfe
Kaitlyn <katiefujihara@gmail.com>
2018-03-21 23:14:04 -0700

    do all of the css things

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ function getInfo(user) {
       $('#showUsers').text('');
       $("#resultsFound").text(matches.total_count + " Results Found");
       matches.items.forEach(function(match) {
-        $('#showUsers').append(`<div class="col-md-4"><a href="${match.html_url}"><h3>${match.login}</h3></a><img src="${match.avatar_url}"<h4>${match.name}</h4>`);
+        $('#showUsers').append(`<div class="col-md-4"><a href="${match.html_url}"><h3>${match.login}</h3></a><img src="${match.avatar_url}" id="profilepic"><h4>${match.name}</h4>`);
       });
     });
   });
@@ -17,16 +17,22 @@ function getInfo(user) {
 
 $(document).ready(function() {
   $('#nameForm').submit(function(event) {
-    event.preventDefault();
+    $("#git").show();
+    $("#codelines").hide();
     let searchString = $('#username').val();
     $('#username').val("");
     search(searchString, function(response) {
       $('#showUsers').text('');
-      $("#resultsFound").text(response.total_count + " Results Found");
+      $("#resultsFound").text(response.total_count + " Result(s) Found");
       response.items.forEach(function(user) {
-        $('#showUsers').append(`<div class="col-md-4"><a href="${user.html_url}"><h3>${user.login}</h3></a><img src="${user.avatar_url}"<p>${user.login}</p><button type="button" class="btn btn-info matchMe">Match Me!</button><hr></div>`);
+        $('#showUsers').append(`<div class="col-md-4" id="me"><a href="${user.html_url}"><h3>${user.login}</h3></a><img src="${user.avatar_url}" id="profilepic"><button type="button" class="btn btn-info matchMe">Match Me!</button><hr></div>`);
         $('.matchMe').last().click(function() {getInfo(user)});
       });
     });
+    event.preventDefault();
+  });
+  $("#matchrequest").click(function(event){
+    $("#git").hide();
+    $("#codelines").show();
   });
 });
